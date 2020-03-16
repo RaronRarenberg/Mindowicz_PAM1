d392ae506fcc2864ead6d231ea828a84863e4540
kmindowicz <301486@uwr.edu.pl>
2020-03-16 11:40:20 +0100

    Można już pobrać plik

--- Project files/app/src/main/java/com/raronrarenberg/konwerterwalut/MainActivity.java
+++ Project files/app/src/main/java/com/raronrarenberg/konwerterwalut/MainActivity.java
@@ -2,13 +2,34 @@ package com.raronrarenberg.konwerterwalut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    long id_w_kolejce;
+    DownloadManager dm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void Pobierz_Waluty(View widok)
+    {
+        dm = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse("https://www.nbp.pl/kursy/xml/a051z200313.xml");
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        Long reference = dm.enqueue(request);
     }
 }
